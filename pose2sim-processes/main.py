1a10da8da7aaaffd7a31fc8fb31a8e45f86ef6e8
mattorga <mattheworga85@gmail.com>
2024-09-24 10:11:45 +0800

    Kinematics Update

--- pose2sim-processes/main.py
+++ pose2sim-processes/main.py
@@ -8,14 +8,15 @@ from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication
 from qt_material import apply_stylesheet
 
 from Pose2Sim import Pose2Sim
+from utils import pose2sim_custom
 from ui_process import Ui_MainWindow
 
 # Hardcoded for the meantime
 root_path = "/Users/mattheworga/Documents/Git/DLSU/thesis/database"
 
-session_folder = "S00_Demo_Batch"
-participant_folder = "S00_P00"
-trial_folder = "Trial_1"
+session_folder = "S00_Demo-Batch"
+participant_folder = "P00_Demo-man"
+trial_folder = "T00_Trial"
 
 session_config_path = os.path.join(root_path, session_folder, "Config.toml")
 session_config_dict = toml.load(session_config_path)
@@ -36,15 +37,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
   def initUI(self):
     self.sessionFolderText.setText(session_folder)
     self.trialFolderText.setText(trial_folder)
+  
+    self.processButton.clicked.connect(lambda: pose2sim_custom.calibration(session_config_dict))
 
-    self.processButton.clicked.connect(lambda: Pose2Sim.calibration(session_config_dict))
-
-    self.poseEstimationButton.clicked.connect(lambda: Pose2Sim.poseEstimation(trial_config_dict))
-    self.synchronizationButton.clicked.connect(lambda: Pose2Sim.synchronization(trial_config_dict))
+    self.poseEstimationButton.clicked.connect(lambda: pose2sim_custom.poseEstimation(trial_config_dict))
+    self.synchronizationButton.clicked.connect(lambda: pose2sim_custom.synchronization(trial_config_dict))
     self.personAssociationButton.clicked.connect(lambda: Pose2Sim.personAssociation(trial_config_dict))
     self.triangulationButton.clicked.connect(lambda: Pose2Sim.triangulation(trial_config_dict))
     self.filteringButton.clicked.connect(lambda: Pose2Sim.filtering(trial_config_dict))
     self.markerAugmentationButton.clicked.connect(lambda: Pose2Sim.markerAugmentation(trial_config_dict))
+    self.kinematicsButton.clicked.connect(lambda: Pose2Sim.kinematics(trial_config_dict))
 
     self.threadCheckButton.clicked.connect(self.handleThreadCheckButton)
 
