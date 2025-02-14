0ffdc57fe35ea27a3da1258ab792a1466f264b33
carlosmolo <82690190+carlosmolo@users.noreply.github.com>
2025-02-14 20:19:30 +0800

    Add files via upload

---
+++ camera_manager.py
@@ -0,0 +1,137 @@
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtGui import QImage, QPixmap
+import cv2
+import os
+
+class Camera(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+
+    def __init__(self, camera_number=0):
+        super().__init__()
+        self.camera_number = camera_number
+        self.threadActive = True
+
+    def run(self):
+        self.isRecording = False
+        self.videoWriter = None
+
+        cap = cv2.VideoCapture(self.camera_number)
+        if not cap.isOpened():
+            print(f"Error: Could not open camera {self.camera_number}")
+            return
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        
+        # After cap = cv2.VideoCapture(self.camera_number)
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        while self.threadActive:
+            ret, frame = cap.read()
+            if ret:
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                if self.isRecording:
+                    if self.videoWriter is None:
+                        filename = self.get_unique_filename(self.camera_number)
+                        self.videoWriter = cv2.VideoWriter(
+                            filename, 
+                            fourcc, 
+                            30.0, 
+                            (frame_width, frame_height)
+                        )
+                    self.videoWriter.write(frame)
+
+                flippedImage = cv2.flip(image, 1)
+                ConvertToQtFormat = QImage(
+                    flippedImage.data,            
+                    flippedImage.shape[1], 
+                    flippedImage.shape[0],
+                    QImage.Format_RGB888
+                )
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(Pic)
+                
+        if self.videoWriter is not None:
+            self.videoWriter.release()
+        cap.release()
+
+    def stop(self):
+        self.threadActive = False
+        if self.videoWriter is not None:
+            self.videoWriter.release()
+        self.wait()
+
+    def start_recording(self):
+        self.isRecording = True
+
+    def stop_recording(self):
+        self.isRecording = False
+        if self.videoWriter is not None:
+            print(f"{self.camera_number} stopped recording...")
+            self.videoWriter.release()
+            self.videoWriter = None
+
+    def get_unique_filename(self, base_number):
+        counter = 0
+        while True:
+            if counter == 0:
+                filename = f'output_{base_number}.avi'
+            else:
+                filename = f'output_{base_number}_{counter}.avi'
+                
+            if not os.path.exists(filename):
+                return filename
+            counter += 1
+        
+class CameraManager:
+    def __init__(self, main_window):
+        self.main_window = main_window
+        self.camera_workers = {}
+        self.camera_slots = {
+            0: main_window.ui.cameraSlot1,
+            1: main_window.ui.cameraSlot2,
+            2: main_window.ui.cameraSlot3
+        }
+        self.available_cameras = []
+        self.framerates = []
+
+    def detect_available_cameras(self, max_cameras=3):
+        for i in range(max_cameras):
+            cap = cv2.VideoCapture(i)
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            if cap.isOpened():
+                ret, _ = cap.read()
+                if ret:
+                    self.available_cameras.append(i)
+                    self.framerates.append(fps)
+            cap.release()
+            
+        self.main_window.ui.camerasValue.setText(str(len(self.available_cameras)))
+        self.main_window.ui.framerateValue.setText(str(self.framerates))
+        
+        for camera_index in self.available_cameras:
+            worker = Camera(camera_index)
+            worker.ImageUpdate.connect(
+                lambda image, idx=camera_index: self.update_camera_feed(image, idx)
+            )
+            worker.start()
+            self.camera_workers[camera_index] = worker
+
+    def update_camera_feed(self, image, camera_index):
+        self.camera_slots[camera_index].setPixmap(QPixmap.fromImage(image))
+
+    def close_all_cameras(self):
+        for worker in self.camera_workers.values():
+            worker.stop()
+            worker.wait() 
+        self.camera_workers.clear()
+        self.available_cameras.clear()
+        self.main_window.ui.camerasValue.setText(str(len(self.available_cameras)))
+
+    def start_recording_all_cameras(self):
+        for worker in self.camera_workers.values():
+            worker.start_recording()
+
+    def stop_recording_all_cameras(self):
+        for worker in self.camera_workers.values():
+            worker.stop_recording()
---
+++ main.py
@@ -0,0 +1,156 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+import os
+#change line below
+from ui_final import Ui_MainWindow
+
+from camera_manager import Camera, CameraManager
+from directory_manager import DirectoryManager
+#change line below
+from ui_patient_form import Ui_patient_form
+
+class MainWindow(QMainWindow):
+  def __init__(self):
+    super(MainWindow, self).__init__()
+    
+    self.ui = Ui_MainWindow()
+    self.ui.setupUi(self)
+
+    self.ui.stackedWidget.setCurrentIndex(0)
+    
+    self.camera_manager = CameraManager(self)
+    self.directory_manager = DirectoryManager(self)
+
+    self.setup_connections()
+ 
+  # --- Page Changing Functions --- #
+  # Note: Simple enough to not need signal/slot implementation
+  def on_dashboardButton_clicked(self):
+    self.ui.stackedWidget.setCurrentIndex(0)
+  def on_camerasButton_clicked(self):
+    self.ui.stackedWidget.setCurrentIndex(1)
+  def on_simulationButton_clicked(self):
+    self.ui.stackedWidget.setCurrentIndex(2)
+  def on_jointAnalyticsButton_clicked(self):
+    self.ui.stackedWidget.setCurrentIndex(3)
+
+  # --- Exit Program --- #
+  def on_exitButton_clicked(self):
+    self.close()
+  
+  # --- Directory and Page Setup --- #
+  def setup_connections(self):      
+    # User Selection
+    self.ui.sessionSelectButton.clicked.connect(self.on_select_session)
+    self.ui.participantSelectButton.clicked.connect(self.select_participant)
+    self.ui.trialSelectButton.clicked.connect(self.select_trial)
+
+    # User Functions
+    self.ui.participantSelectButton.clicked.connect(self.add_participant)
+
+    # Camera Page
+    self.ui.detectCamerasButton.clicked.connect(self.detect_cameras)
+    self.ui.closeCamerasButton.clicked.connect(self.close_cameras)
+    self.ui.startRecordingButton.clicked.connect(self.start_recording)
+    self.ui.stopRecordingButton.clicked.connect(self.stop_recording)
+
+  # --- User Selection Functions --- #
+  def on_select_session(self):
+    self.directory_manager.set_session()
+    session_name = self.directory_manager.get_session()
+    
+    # Update UI
+    self.ui.sessionSelectedLabel.setText(session_name)
+    self.ui.participantSelectButton.setEnabled(True)
+    self.ui.participantAddButton.setEnabled(True)
+
+    self.ui.sessionSelectedLabel.setText(session_name)
+    self.ui.participantSelectButton.setEnabled(True)
+    self.ui.participantAddButton.setEnabled(True)
+
+
+  def select_participant(self):
+    if not self.sessionDirectory:
+        return
+        
+    dialog = QFileDialog(self)
+    dialog.setFileMode(QFileDialog.Directory)
+    dialog.setOption(QFileDialog.ShowDirsOnly, True)
+    dialog.setDirectory(self.sessionDirectory)
+    
+    if dialog.exec_():
+        selected_path = dialog.selectedFiles()[0]
+        # Verify selected folder is directly under session folder
+        if os.path.dirname(selected_path) == self.sessionDirectory:
+            self.participantDirectory = selected_path
+            participant_name = os.path.basename(selected_path)
+            
+            # Update UI
+            self.ui.participantSelectedLabel.setText(participant_name)
+            self.ui.trialSelectButton.setEnabled(True)
+            self.ui.trialAddButton.setEnabled(True)
+
+            print(self.participantDirectory)
+            
+            # Reset trial selection
+            self.trialDirectory = None
+            self.ui.trialSelectedLabel.setText("")
+        else:
+            QMessageBox.warning(self, "Invalid Selection", 
+                              "Please select a participant folder directly under the session folder.")
+  def select_trial(self):
+    if not self.participantDirectory:
+        return
+        
+    dialog = QFileDialog(self)
+    dialog.setFileMode(QFileDialog.Directory)
+    dialog.setOption(QFileDialog.ShowDirsOnly, True)
+    dialog.setDirectory(self.participantDirectory)
+    
+    if dialog.exec_():
+        selected_path = dialog.selectedFiles()[0]
+        # Verify selected folder is directly under participant folder
+        if os.path.dirname(selected_path) == self.participantDirectory:
+            self.trialDirectory = selected_path
+            trial_name = os.path.basename(selected_path)
+            
+            # Update UI
+            self.ui.trialSelectedLabel.setText(trial_name)
+            
+            print(self.trialDirectory)
+
+        else:
+            QMessageBox.warning(self, "Invalid Selection", 
+                              "Please select a trial folder directly under the participant folder.")
+  def add_participant(self):
+    dialog = QDialog(self)  # Create dialog with main window as parent
+    form = Ui_patient_form()  # Create the form
+    form.setupUi(dialog)  # Setup the form UI on the dialog
+    
+    # Show the dialog modally and get result
+    if dialog.exec_() == QDialog.Accepted:
+        first_name = form.firstNameField.text()
+        last_name = form.lastNameField.text()
+        height = form.heightField.text()
+        weight = form.weightField.text()
+        
+  # --- Cameras Page functions --- #
+  def detect_cameras(self):
+    self.camera_manager.detect_available_cameras()
+  def close_cameras(self):
+    self.camera_manager.close_all_cameras()
+  def start_recording(self):
+    self.camera_manager.start_recording_all_cameras() 
+  def stop_recording(self):
+    self.camera_manager.stop_recording_all_cameras()
+
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+
+  window = MainWindow()
+  window.show()
+
+  sys.exit(app.exec()) 
