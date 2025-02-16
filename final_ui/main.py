d5ca40ef0d42e017ac3b82df60d5aba6f6e522ac
mattorga <mattheworga85@gmail.com>
2025-02-16 12:29:16 +0800

    Tested Pose2Sim Processing Feature

--- final_ui/main.py
+++ final_ui/main.py
@@ -3,6 +3,9 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+from Pose2Sim import Pose2Sim
+import toml
+
 import os
 
 from final import Ui_MainWindow
@@ -42,13 +45,13 @@ class MainWindow(QMainWindow):
   def on_simulationButton_clicked(self):
     self.ui.stackedWidget.setCurrentIndex(2)
   def on_jointAnalyticsButton_clicked(self):
-    self.ui.stackedWidget.setCurrentIndex(3)
+    self.ui.stackedWidget.setCurrentIndex(3) 
 
   # --- Exit Program --- #
   def on_exitButton_clicked(self):
     self.close()
   
-  # --- Directory and Page Setup --- #
+  # --- Directory, Page, Process Setup --- #
   def setup_connections(self):      
     # User Selection
     self.ui.sessionSelectButton.clicked.connect(self.on_select_session)
@@ -64,6 +67,9 @@ class MainWindow(QMainWindow):
     self.ui.closeCamerasButton.clicked.connect(self.on_close_cameras)
     self.ui.startRecordingButton.clicked.connect(self.on_start_recording)
     self.ui.stopRecordingButton.clicked.connect(self.on_stop_recording)
+
+    # Process Button
+    self.ui.processButton.clicked.connect(self.on_process)
 
   # --- User Selection Functions --- #
   def on_select_session(self):
@@ -81,6 +87,8 @@ class MainWindow(QMainWindow):
       self.ui.trialSelectedLabel.setText("")
       self.ui.trialSelectButton.setEnabled(False)
       self.ui.trialAddButton.setEnabled(False)
+
+      self.ui.processButton.setEnabled(False)
   def on_select_participant(self):
     self.directory_manager.set_participant()
     participant_name = self.directory_manager.participant_dir
@@ -95,6 +103,7 @@ class MainWindow(QMainWindow):
 
     if trial_name:
       self.ui.trialSelectedLabel.setText(trial_name)
+      self.ui.processButton.setEnabled(True)
   def add_participant(self):
     self.directory_manager.add_participant()
   def add_trial(self):
@@ -107,15 +116,42 @@ class MainWindow(QMainWindow):
     # Update UI elements
     self.ui.camerasValue.setText(str(self.camera_manager.camera_count))
     self.ui.framerateValue.setText(str(self.camera_manager.framerates))
-
   def on_close_cameras(self):
     self.camera_manager.close_all_cameras()
     self.ui.camerasValue.setText(str(self.camera_manager.camera_count))
-
   def on_start_recording(self):
     self.camera_manager.start_recording_all_cameras() 
   def on_stop_recording(self):
     self.camera_manager.stop_recording_all_cameras()
+
+  def on_process(self):
+    print(f"Process {self.directory_manager.trial_path}")
+
+    session_config_path = os.path.join(self.directory_manager.session_path, "Config.toml")
+    session_config_dict = toml.load(session_config_path)
+    session_config_dict.get("project").update({"project_dir":os.path.join(self.directory_manager.session_path)})
+    
+    trial_config_path = os.path.join(self.directory_manager.trial_path, "Config.toml")
+    trial_config_dict = toml.load(trial_config_path)
+    trial_config_dict.get("project").update({"project_dir":self.directory_manager.trial_path})
+
+    # Pose2Sim.calibration(session_config_dict) # Working
+    # print("WORKING: Calibration")
+
+    Pose2Sim.poseEstimation(trial_config_dict) # Working
+    print("WORKING: Pose Estimation")
+    Pose2Sim.synchronization(trial_config_dict)
+    print("WORKING: Pose Synchronization")
+    Pose2Sim.personAssociation(trial_config_dict)
+    print("WORKING: Person Association")
+    Pose2Sim.triangulation(trial_config_dict)
+    print("WORKING: Triangulation")
+    Pose2Sim.filtering(trial_config_dict)
+    print("WORKING: Filtering")
+    Pose2Sim.markerAugmentation(trial_config_dict)
+    print("WORKING: Marker Augmentation")
+    Pose2Sim.kinematics(trial_config_dict)
+    print("WORKING: Kinematics")
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
